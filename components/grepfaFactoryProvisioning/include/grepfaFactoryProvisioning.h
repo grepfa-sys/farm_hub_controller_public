0d6832a54502d1169803a488ce088d10cbfbc3b3
vl0011 <vl0011@naver.com>
2023-08-08 03:53:16 +0900

    EDIT unuse include

--- components/grepfaFactoryProvisioning/include/grepfaFactoryProvisioning.h
+++ components/grepfaFactoryProvisioning/include/grepfaFactoryProvisioning.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <stdio.h>
-#include <inttypes.h>
 #include "nvs_flash.h"
 #include "nvs.h"
 #include "nvs_handle.hpp"
@@ -12,19 +10,9 @@
 
 
 class GrepfaFactoryProvisioning {
-    static std::unique_ptr<nvs::NVSHandle> handle;
-public:
-    static char *getBrokerEndpoint();
-
-    static char *getDeviceId();
-
-    static char *getPop();
-
-    static char *getUsername();
-
-    static char *getServiceKey();
-
 private:
+    static std::unique_ptr<nvs::NVSHandle> handle;
+
     static char* broker_endpoint;
     static char* device_id;
     static char* pop;
@@ -36,8 +24,11 @@ private:
     static size_t pop_len;
     static size_t username_len;
     static size_t service_key_len;
-
-
 public:
     static esp_err_t Init();
+    static char *getBrokerEndpoint();
+    static char *getDeviceId();
+    static char *getPop();
+    static char *getUsername();
+    static char *getServiceKey();
 };
